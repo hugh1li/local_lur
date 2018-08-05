3bdf3e59131ef470b573cc372868ca15168fc4a4
Hugh Li <Zhongju1.li@gmail.com>
2018-08-05 15:17:47 -0400

    calculate partial adj r2 based on the adj r2 difference

---
+++ partial_R2.R
@@ -0,0 +1,10 @@
+COA_lm_full <- lm(formula("COA ~  + PointDe_Rest_100meters + Elevation"), sx)
+
+COA_lm_source <- lm(formula("COA ~  + PointDe_Rest_100meters + LUCOMM1000"), sx)
+
+HOA_lm_full <- lm(formula("HOA ~  + TRKDENALL100 + LUAGRI500 + EucDistinv_PM + LUINDUS5000 + ALLDIESAADT_DIS2"), sx)
+
+HOA_lm_source <- lm(formula("HOA ~  + TRKDENALL100 + TRKDENSMAJ1000 + DISTINVALL2"), sx)
+
+chi_lm <- lm(formula( "chi ~  + TRKDENSMAJ1000 + HOUSDEN300 + PointDe_NEI_30000"), sx)
+
